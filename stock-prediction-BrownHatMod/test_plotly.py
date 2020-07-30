abdd9e66aecb989f1c47c59698a634e8ff62d5e9
Ashwin <ashwinvenkatram@gmail.com>
2020-07-30 20:56:52 +0800

    added zigzag implementation

---
+++ stock-prediction-BrownHatMod/test_plotly.py
@@ -0,0 +1,208 @@
+from stock_prediction import create_model, load_data, np
+from parameters import *
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, mean_squared_error
+
+import pickle
+import numpy as np
+from math import sqrt
+from backtest import *
+import sys
+import os
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+import backtest
+import webbrowser
+
+global processed
+
+# def plot_graph(y_pred, y_test, r2_score):
+#     OUTPUT_PATH = "./results/" + model_name
+# 
+#     figTest = plt.figure()
+#     plt.style.use('seaborn-whitegrid')
+#     x_val = [i for i in range(len(y_pred))]
+#     plt.scatter(x_val,y_test[:], c='b',marker="o", s=3)
+#     plt.scatter(x_val,y_pred[:], c='r', marker="o", s=3)
+#     plt.plot(y_test[:], 'b')
+#     plt.plot(y_pred[:], 'r')
+#     plt.xlabel("Days")
+#     plt.ylabel("Price")
+#     plt.legend(["Actual Price", "Predicted Price"])
+#     plt.title("Price Prediction Accuracy, R2: " + str(r2_score))
+# 
+#     # with open(f"results/prediction_{model_name}.csv",'w') as outfile:
+#     #     header = "x_val,y_pred\n"
+#     #     outfile.write(header)
+#     #     for i in range(len(x_val)):
+#     #         outfile.write(f"{x_val[i]},{y_pred[i]}"+"\n")
+#     # outfile.close()
+# 
+#     # pickle.dump(figTest, open(OUTPUT_PATH +'_pred_vs_real' + '.pickle', 'wb'))
+#     # plt.savefig(OUTPUT_PATH +'_pred_vs_real' + '.png')
+#     plt.show()
+
+def plotly_graph(y_pred, y_test, r2_score):
+    dates = processed['df']['date']
+    openData = processed['df']['Open']
+    highData = processed['df']['High']
+    lowData = processed['df']['Low']
+    closeData = processed['df']['Close']
+
+    fig = make_subplots(
+        rows=2, cols=1,
+        shared_xaxes=True,
+        vertical_spacing=0.05,
+        subplot_titles=("Equity", f"{ticker}-Candlestick Chart")
+    )
+
+    fig.add_trace(
+        go.Scatter(x=dates, y=closeData),
+        row=1, col=1
+    )
+
+    # http://127.0.0.1:50979/
+    fig.add_trace(
+        go.Candlestick(x=dates, open=openData, high=highData,low=lowData,close=closeData),
+        row=2, col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(x=dates, y=closeData + 30, mode="markers", marker=dict(color='rgba(244,96,96, 1)', size=8, symbol="triangle-down")),
+        row=2, col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(x=dates, y=openData - 30, mode="markers", marker=dict(color='rgba(50,184,50,1)', size=8, symbol="triangle-up")),
+        row=2, col=1
+    )
+
+    fig.update_layout(
+        autosize=False,
+        width=1500,
+        height=1500,
+        margin=dict(
+            l=50,
+            r=50,
+            b=100,
+            t=100,
+            pad=4
+        )
+    )
+
+    fig.show(renderer="iframe")
+    graphPath = "file://" + os.getcwd() + "/iframe_figures/figure_0.html"
+    webbrowser.open(graphPath)
+
+    # figTest = plt.figure()
+    # plt.style.use('seaborn-whitegrid')
+    # x_val = [i for i in range(len(y_pred))]
+    # plt.scatter(x_val,y_test[:], c='b',marker="o", s=3)
+    # plt.scatter(x_val,y_pred[:], c='r', marker="o", s=3)
+    # plt.plot(y_test[:], 'b')
+    # plt.plot(y_pred[:], 'r')
+    # plt.xlabel("Days")
+    # plt.ylabel("Price")
+    # plt.legend(["Actual Price", "Predicted Price"])
+    # plt.title("Price Prediction Accuracy, R2: " + str(r2_score))
+    #
+    # plt.show()
+
+def prediction(model, X_test, y_test,dataset):
+    y_pred = model.predict(X_test)
+    print(len(X_test), len(y_pred))
+    y_test = np.squeeze(dataset["column_scaler"]["close"].inverse_transform(np.expand_dims(y_test, axis=0)))
+    y_pred = np.squeeze(dataset["column_scaler"]["close"].inverse_transform(y_pred))
+
+    return y_test, y_pred
+
+def slidingWindowPred(model, dataset):
+    y_test = dataset["y_test"]
+    X_test = dataset["X_test"]
+    print("before prediction")
+    y_testInv, y_predInv = prediction(model, X_test, y_test,dataset)
+
+    print("after prediction")
+    # r2_score = get_accuracy(model, data)
+    r2_score = calcR2(y_testInv,y_predInv)
+    print("R2 Score:", r2_score)
+    plotly_graph(y_predInv, y_testInv, r2_score)
+
+def get_accuracy(model, dataset):
+    y_test = dataset["y_test"]
+    X_test = dataset["X_test"]
+    y_pred = model.predict(X_test)
+    y_test = np.squeeze(dataset["column_scaler"]["close"].inverse_transform(np.expand_dims(y_test, axis=0)))
+    y_pred = np.squeeze(dataset["column_scaler"]["close"].inverse_transform(y_pred))
+    y_pred = list(map(lambda current, future: int(float(future) > float(current)), y_test[:-LOOKUP_STEP], y_pred[LOOKUP_STEP:]))
+    y_test = list(map(lambda current, future: int(float(future) > float(current)), y_test[:-LOOKUP_STEP], y_test[LOOKUP_STEP:]))
+    return sqrt(mean_squared_error(y_test, y_pred))
+
+def calcR2(y_test,y_pred):
+    E1 = np.sum(np.multiply(y_test,y_pred)) # Sum(xy)
+    E2 = np.sum(y_test) # Sum of all x
+    E3 = np.sum(y_pred) # Sum of all y
+    n = len(y_pred)
+    E4 = np.sum(np.multiply(y_test,y_test)) # Sum of squares of x
+    E5 = np.sum(np.multiply(y_pred, y_pred)) # sum of squares of y
+
+    R2 = ((n*E1) - (E2 * E3))/(np.sqrt((n*E4 - np.power(E2,2)) * (n*E5 - np.power(E3,2))))
+    return R2
+# def predict(model, data, classification=False):
+#     # retrieve the last sequence from data
+#     last_sequence = data["last_sequence"][:N_STEPS]
+#     print("last_sequence: ", last_sequence)
+#     # retrieve the column scalers
+#     column_scaler = data["column_scaler"]
+#
+#     # reshape the last sequence
+#     last_sequence = last_sequence.reshape((last_sequence.shape[1], last_sequence.shape[0]))
+#
+#     # expand dimension
+#     last_sequence = np.expand_dims(last_sequence, axis=0)
+#
+#     # get the prediction (scaled from 0 to 1)
+#     prediction = model.predict(last_sequence)
+#     # get the price (by inverting the scaling)
+#     # predicted_price = column_scaler["close"].inverse_transform(prediction)[0][0]
+#     predicted_price = column_scaler["close"].inverse_transform(prediction)
+#     return predicted_price
+
+if __name__ == "__main__":
+    # load the data
+    # data = load_data(ticker, N_STEPS, lookup_step=LOOKUP_STEP, test_size=TEST_SIZE,
+    #                 feature_columns=FEATURE_COLUMNS, shuffle=False)
+    data, processed = backtest.load_data(ticker, N_STEPS, True, lookup_step=LOOKUP_STEP, feature_columns=FEATURE_COLUMNS)
+
+    # construct the model
+    model = create_model(N_STEPS, loss=LOSS, units=UNITS, cell=CELL, n_layers=N_LAYERS,
+                        dropout=DROPOUT, optimizer=OPTIMIZER, bidirectional=BIDIRECTIONAL)
+
+    model_path = os.path.join("results", model_name) + ".h5"
+    model.load_weights(model_path)
+
+    # slidingWindowPred(model, data)
+    print("before sliding window")
+    slidingWindowPred(model, processed)
+
+#  try test.py with marker ^ to mark buy/ sell conditions over time. visualize the system on test plot instead of backtest.plot()
+#  Correlation of pred with buy sell condition not clear
+
+# # evaluate the model
+# mse, mae = model.evaluate(data["X_test"], data["y_test"], verbose=0)
+# # calculate the mean absolute error (inverse scaling)
+#
+# mean_square_error = data["column_scaler"]["adjclose"].inverse_transform([[mse]])[0][0]
+# mean_absolute_error = data["column_scaler"]["adjclose"].inverse_transform([[mae]])[0][0]
+# print("Mean Absolute Error:", mean_absolute_error)
+# print("Mean Square Error:", mean_square_error)
+
+# predict the future price
+# future_price = predict(model, data)
+# print(future_price)
+# print(f"Future price after {LOOKUP_STEP} days is {future_price:.2f}$")
+# r2_score = get_accuracy(model, data)
+# print("R2 Score:", r2_score)
+# plot_graph(model, data, r2_score)
