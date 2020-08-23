bugfixes. stationary close formula and plot shift of 1 bug

stock-prediction-BrownHatMod/stock_prediction.py
@@ -12,10 +12,13 @@ from yahoo_fin import stock_info as si
 from yfinance import *
 from collections import deque
 
+import math
 import numpy as np
 import pandas as pd
 import random
 from parameters import *
+from pickle import dump
+import os
 from datetime import date, timedelta
 import sys
 
@@ -68,7 +71,7 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=False, lookup_step=1,
     '''
     FOR BINANCE DATA PULLING
     '''
-    df = get_all_binance(ticker, INTERVAL, save=False)
+    df = get_all_binance(ticker, INTERVAL, save=True)
     result = {}
     '''
     REST OF CODE
@@ -84,26 +87,30 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=False, lookup_step=1,
     # Subtract previous data from current data by shifting the column down by 1
     df['Open_logDiff'] = np.log(df['Open'.lower()]) - np.log(df['Open'.lower()]).shift(1)
     df['Close_logDiff'] = np.log(df['Close'.lower()]) - np.log(df['Close'.lower()]).shift(1)
-
     df['High_logDiff'] = np.log(df['High'.lower()]) - np.log(df['High'.lower()]).shift(1)
     df['Low_logDiff'] = np.log(df['Low'.lower()]) - np.log(df['Low'.lower()]).shift(1)
     df['Volume_logDiff'] = np.log(df['volume'.lower()]) - np.log(df['volume'.lower()]).shift(1)
 
-    shiftedOpen = df['Open_logDiff'].shift(-lookup_step)
-    df.insert(column='NextOpen_logDiff', value=shiftedOpen, loc=len(df.columns))
+    # shiftedOpen = df['Open_logDiff'].shift(-lookup_step)
+    # df.insert(column='NextOpen_logDiff', value=shiftedOpen, loc=len(df.columns))
 
     # drop =/- inf by setting to NaN
     df = df[~df.isin([np.nan, np.inf, -np.inf]).any(1)]
     df.dropna(inplace=True)
 
-    # contains the full datafram of OHLCV and computing stationary dataset
-    result['df'] = df.copy()
+    # contains the full dataframe of OHLCV after dropped datapoints and computing stationary dataset
+    # result['df'] = df.copy()
 
     # scaling to normalize stationary dataset from 0 to 1
     if scale:
         column_scaler = {}
         # scale the data (prices) from 0 to 1
-        feature_columns = ["Open_logDiff", "NextOpen_logDiff","High_logDiff", "Low_logDiff", "Close_logDiff","Volume_logDiff"]
+        # feature_columns = ["Open_logDiff", "NextOpen_logDiff","High_logDiff", "Low_logDiff", "Close_logDiff","Volume_logDiff"]
+        feature_columns = ["Open_logDiff", "High_logDiff", "Low_logDiff", "Close_logDiff",
+                           "Volume_logDiff"]
+
+        scaler = preprocessing.MinMaxScaler()
+
         for column in feature_columns:
             scaler = preprocessing.MinMaxScaler()
             df[column] = scaler.fit_transform(np.expand_dims(df[column].values, axis=1))
@@ -111,35 +118,149 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=False, lookup_step=1,
 
         # add the MinMaxScaler instances to the result returned
         result["column_scaler"] = column_scaler
+        dump(scaler, open(os.path.join("results", 'scaler-' + model_name + '.pickle'), 'wb'))
 
     # add the target column (label) by shifting by `lookup_step`
-    df['future'] = df['Close_logDiff'].shift(-lookup_step)
-
-    last_sequence = np.array(df[feature_columns].tail(lookup_step))
+    df['future'] = df['Close_logDiff'].shift(-LOOKUP_STEP)
+    # colsList = df.columns.tolist()
+    # colsList = colsList.remove('date')
+    # last_sequence = np.array(df[colsList].tail(lookup_step))
 
     # drop NaNs
+    # df = df[~df.isin([np.nan, np.inf, -np.inf]).any(1)]
     df.dropna(inplace=True)
 
-    sequence_data = []
-    sequences = deque(maxlen=n_steps)
+    # At this point df of stationary OHLCV, groundtruth predClose has been created
+    dfTrain, dfTest = mydata_test_train_split(df, TEST_SIZE)
+
+    # contains the full dataframe of OHLCV after dropped datapoints and computing stationary dataset
+    result['df'] = df.copy()
+    result['dfTrain'] = dfTrain.copy()
+    result['dfTest'] = dfTest.copy()
+
+    # scaling
+    # if scale:
+    #     result, dfTrain, scaler = scalingTrain(dfTrain, result)
+    #     result, dfTest = scalingTest(dfTest,scaler, result)
+    #
+    #     dump(scaler, open(os.path.join("results", 'scaler-'+model_name+'.pickle'),'wb'))
+
+    # creating sequence data from stationary and scaled dataset
+    X_Train, Y_Train = create_sequence_data(dfTrain)
+    X_Test, Y_Test = create_sequence_data(dfTest)
+
+    # sequence_data = []
+    # sequences = deque(maxlen=n_steps)
     # [df['future'].values, df['Close'.lower()].shift(-lookup_step)]
     # entry: input to RNN, stationary and normalized OHLCV
     # target: groundtruth close data (stationary and normalized) -> C_sn
     # target2: groundtruth close data (ha close or close depending on setting) -> C
 
-    for entry, target, target2 in zip(df[feature_columns].values, df['future'].values,  df['Close'.lower()].shift(-lookup_step)):
-        sequences.append(entry)
-        if len(sequences) == n_steps:
-            sequence_data.append([np.array(sequences), target, target2])
+    # feature_columns = ["Open_logDiff", "High_logDiff", "Low_logDiff", "Close_logDiff","Volume_logDiff"]
+    # for entry, target, target2 in zip(df[feature_columns].values, df['future'].values,  df['Close'.lower()]):# -lookup_step.shift()
+    #     sequences.append(entry)
+    #     if len(sequences) == n_steps:
+    #         # At Nth step, N X_train/test values exist, with the N+1th target and Nth close correction factor
+    #         sequence_data.append([np.array(sequences), target, target2])
+    #         # print([np.array(sequences), target, target2])
+    #         # break
 
     # get the last sequence by appending the last `n_step` sequence with `lookup_step` sequence
     # for instance, if n_steps=50 and lookup_step=10, last_sequence should be of 59 (that is 50+10-1) length
     # this last_sequence will be used to predict in future dates that are not available in the dataset
-    last_sequence = list(sequences) + list(last_sequence)
+    # last_sequence = list(sequences) + list(last_sequence)
     # shift the last sequence by -1
-    last_sequence = np.array(pd.DataFrame(last_sequence).shift(-1).dropna())
+    # last_sequence = np.array(pd.DataFrame(last_sequence).shift(-1).dropna())
     # add to result
-    result['last_sequence'] = last_sequence
+    # result['last_sequence'] = last_sequence
+
+    # # construct the X's and y's
+    # X, y = [], []
+    # # X: input OHLCV_logDiff and normalized
+    # # Y: (C_sn, C)
+    # for seq, target, target2 in sequence_data:
+    #     X.append(seq)
+    #     y.append([target, target2])
+    #
+    # # convert to numpy arrays
+    # X = np.array(X)
+    # y = np.array(y)
+    #
+    # # reshape X to fit the neural network
+    # X = X.reshape((X.shape[0], X.shape[2], X.shape[1]))
+
+    # split the dataset
+    # result["X_train"], result["X_test"], result["y_train"], result["y_test"] = train_test_split(X, y,
+    #                                                                             test_size=test_size, shuffle=shuffle)
+
+    # result["y_train_logDiffClose"] = result["y_train"][:,1] # shifted close data for recalculation of actual close
+    # result["y_train"] = result["y_train"][:,0] # stationary and normalized data
+    #
+    # result["y_test_logDiffClose"] = result["y_test"][:, 1] # shifted close data for recalculation of actual close
+    # result["y_test"] = result["y_test"][:, 0] # stationary and normalized data
+
+    result["X_train"] = X_Train
+    result["y_train_logDiffClose"] = Y_Train[:, 1]  # shifted close data for recalculation of actual close
+    result["y_train"] = Y_Train[:, 0]  # stationary and normalized data
+
+    result["X_test"] = X_Test
+    result["y_test_logDiffClose"] = Y_Test[:, 1]  # shifted close data for recalculation of actual close
+    result["y_test"] = Y_Test[:, 0]  # stationary and normalized data
+
+    # Assuming not shuffled
+    # picking up values after first 70 datapoints
+    testDataset = pd.DataFrame()
+    testDataset['Timestamp'] = result['df']['timestamp'][-len(result["y_test"]):]
+    testDataset['Open'] = result['df']['open'][-len(result["y_test"]):]
+    testDataset['High'] = result['df']['high'][-len(result["y_test"]):]
+    testDataset['Low'] = result['df']['low'][-len(result["y_test"]):]
+    testDataset['Close'] = result['df']['close'][-len(result["y_test"]):]
+    testDataset['Volume'] = result['df']['volume'][-len(result["y_test"]):]
+
+    # return the result
+    return result, testDataset
+
+def scalingTrain(dfTrain, result):
+    column_scaler = {}
+        # scale the data (prices) from 0 to 1
+        # feature_columns = ["Open_logDiff", "NextOpen_logDiff","High_logDiff", "Low_logDiff", "Close_logDiff","Volume_logDiff"]
+    feature_columns = ["Open_logDiff", "High_logDiff", "Low_logDiff", "Close_logDiff",
+                           "Volume_logDiff", "future"]
+
+    scaler = preprocessing.MinMaxScaler()
+
+    for column in feature_columns:
+        scaler = preprocessing.MinMaxScaler()
+        dfTrain[column] = scaler.fit_transform(np.expand_dims(dfTrain[column].values, axis=1))
+        column_scaler[column] = scaler
+
+        # add the MinMaxScaler instances to the result returned
+    result["column_scaler"] = column_scaler
+    return result, dfTrain, scaler
+
+def scalingTest(dfTest, scaler, result):
+    # scale the data (prices) from 0 to 1
+    # feature_columns = ["Open_logDiff", "NextOpen_logDiff","High_logDiff", "Low_logDiff", "Close_logDiff","Volume_logDiff"]
+    feature_columns = ["Open_logDiff", "High_logDiff", "Low_logDiff", "Close_logDiff",
+                           "Volume_logDiff", "future"]
+    for column in feature_columns:
+        dfTest[column] = scaler.transform(np.expand_dims(dfTest[column].values, axis=1))
+
+    # add the MinMaxScaler instances to the result returned
+    return result, dfTest
+
+def create_sequence_data(df):
+    sequence_data = []
+    sequences = deque(maxlen=N_STEPS)
+
+    feature_columns = ["Open_logDiff", "High_logDiff", "Low_logDiff", "Close_logDiff","Volume_logDiff"]
+    for entry, target, target2 in zip(df[feature_columns].values, df['future'].values,  df['Close'.lower()].values):# df['Close'.lower()]
+        sequences.append(entry)
+        if len(sequences) == N_STEPS:
+            # At Nth step, N X_train/test values exist, with the N+1th target and Nth close correction factor
+            sequence_data.append([np.array(sequences), target, target2])
+            # print([np.array(sequences), target, target2])
+            # break
 
     # construct the X's and y's
     X, y = [], []
@@ -156,30 +277,17 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=False, lookup_step=1,
     # reshape X to fit the neural network
     X = X.reshape((X.shape[0], X.shape[2], X.shape[1]))
 
-    # split the dataset
-    result["X_train"], result["X_test"], result["y_train"], result["y_test"] = train_test_split(X, y,
-                                                                                test_size=test_size, shuffle=shuffle)
-    # print("X_train shape: ",result["X_train"].shape)
-    # print("X_test shape: ", result["X_test"].shape)
-    # print("y_train shape: ", result["y_train"].shape)
-    # print("y_test shape: ", result["y_test"].shape)
+    return X, y
 
-    result["y_train_logDiffClose"] = result["y_train"][:,1] # shifted close data for recalculation of actual close
-    result["y_train"] = result["y_train"][:,0] # stationary and normalized data
+# dataset will be the full df. to be split into test and train respectively
+def mydata_test_train_split(dataset,test_size):
+    train_size = 1 - test_size
+    rows = dataset.shape[0]
 
-    result["y_test_logDiffClose"] = result["y_test"][:, 1] # shifted close data for recalculation of actual close
-    result["y_test"] = result["y_test"][:, 0] # stationary and normalized data
-
-    # Assuming not shuffled
-    testDataset = pd.DataFrame()
-    testDataset['open'] = result['df']['open'][-len(result["y_test"]):]
-    testDataset['high'] = result['df']['high'][-len(result["y_test"]):]
-    testDataset['low'] = result['df']['low'][-len(result["y_test"]):]
-    testDataset['close'] = result['df']['close'][-len(result["y_test"]):]
-    testDataset['volume'] = result['df']['volume'][-len(result["y_test"]):]
-
-    # return the result
-    return result, testDataset
+    trainRows = math.floor(rows * train_size)
+    trainSet = dataset.copy()[:trainRows]
+    testSet = dataset.copy()[trainRows:]
+    return trainSet, testSet
 
 # For non stationary input dataset
 # def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1,

stock-prediction-BrownHatMod/test.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, mean_squared_error
 
 import pickle
+
 import numpy as np
 import pandas as pd
 from math import sqrt
@@ -40,44 +41,73 @@ def plot_graph(y_pred, y_test, r2_score):
 
 def prediction(model, X_test, y_test,dataset):
     y_pred = model.predict(X_test)
-    # print(len(X_test), len(y_pred))
-    # y_test = np.squeeze(dataset["column_scaler"]["close"].inverse_transform(np.expand_dims(y_test, axis=0)))
-    # y_pred = np.squeeze(dataset["column_scaler"]["close"].inverse_transform(y_pred))
 
-    # invert close normalization process
+    # print(len(X_test), len(y_pred))
     y_test = np.squeeze(dataset["column_scaler"]["Close_logDiff"].inverse_transform(np.expand_dims(y_test, axis=0)))
     y_pred = np.squeeze(dataset["column_scaler"]["Close_logDiff"].inverse_transform(y_pred))
+
+    # invert close normalization process
+    # scaler = pickle.load(open(os.path.join("results", 'scaler-'+model_name+'.pickle'),'rb'))
+    # y_test = np.squeeze(scaler.inverse_transform(np.expand_dims(y_test, axis=1)))
+    # y_pred = np.squeeze(scaler.inverse_transform(y_pred))
+
+    # Shape of y_test: (5249,)
+    # Shape of y_pred: (5249, 1)
+    # array([[0.557144 ],
+    #        [0.557144 ],
+    #        [0.557144 ],
+    #        ...,
+    #        [0.557144 ],
+    #        [0.557144 ],
+    #        [0.5571439]], dtype=float32)
+    # Hence expand dimensions along column => axis = 1
+    # y_test = np.squeeze(dataset["column_scaler"]["Close_logDiff"].inverse_transform(np.expand_dims(y_test, axis=1)))
+    # y_pred = np.squeeze(dataset["column_scaler"]["Close_logDiff"].inverse_transform(y_pred))
 
     # df['Close_logDiff'] = np.log(df['Close'.lower()]) - np.log(df['Close'.lower()]).shift(1)
 
     # non-stationary conversion
+    # y_pred = np.squeeze(y_pred)
     y_test = np.exp(y_test + np.log(dataset["y_test_logDiffClose"]))
     y_pred = np.exp(y_pred + np.log(dataset["y_test_logDiffClose"]))
 
     # Removing NaN
-    y_test = y_test[~np.isnan(y_test)]
-    y_pred = y_pred[~np.isnan(y_pred)]
+    # y_test = y_test[~np.isnan(y_test)]
+    # y_pred = y_pred[~np.isnan(y_pred)]
 
     return y_test, y_pred
 
 def slidingWindowPred(model, dataset):
     y_test = dataset["y_test"]
     X_test = dataset["X_test"]
+    y_testOriClose = testDataset['Close'].values # shift not necessary
+
+    # print(X_test.shape)
     y_testInv, y_predInv = prediction(model, X_test, y_test,dataset)
 
     # r2_score = get_accuracy(model, data)
-    r2_score = calcR2(y_testInv,y_predInv)
+    # r2_score = calcR2(y_testInv,y_predInv)
+
+    # Comparing pred with original untouched close data
+    r2_score = calcR2(y_testOriClose, y_predInv)
     print("R2 Score:", r2_score)
 
     # saving OHLCV and predClose to csv
-    y_predInvSave = np.append(y_predInv, [0])
-    # print(testDataset.shape, len(y_predInv2))
-    testDataset['predClose'] = y_predInvSave
-    testDataset.drop(testDataset.tail(1).index, inplace=True)  # drop last n rows
+    compDF = pd.DataFrame()
+    compDF.insert(column='close', value=y_testOriClose, loc=len(compDF.columns))
+    compDF.insert(column='y_predInv', value=y_predInv, loc=len(compDF.columns))
+    # OHLCV, nextPredClose for purpose of backtesting
+
+    testDataset['predClose'] = compDF['y_predInv'].values
+    testDataset['predClose'] = testDataset['predClose'].shift(-1) # For Pranav to compare each close with corresponding next close
+
+    testDataset.dropna(inplace=True)
+
     filename = './backtest/BTCUSDT/BTCUSDT_OHLCVpC.csv'
     testDataset.to_csv(filename)
 
-    plot_graph(y_predInv, y_testInv, r2_score)
+    # plot_graph(y_predInv, y_testInv, r2_score)
+    plot_graph(compDF['y_predInv'].values, compDF['close'].values, r2_score)
 
 def get_accuracy(model, dataset):
     y_test = dataset["y_test"]
@@ -122,7 +152,6 @@ def calcR2(y_test,y_pred):
 #     # predicted_price = column_scaler["close"].inverse_transform(prediction)[0][0]
 #     predicted_price = column_scaler["close"].inverse_transform(prediction)
 #     return predicted_price
-
 
 # load the data
 data, testDataset = load_data(ticker, N_STEPS, lookup_step=LOOKUP_STEP, test_size=TEST_SIZE,